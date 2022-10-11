6125ce75a00713dcf65fb1e1f1dd9733d46ee456
sammy freddie <sammyfreddie@sammys-MacBook-Pro.local>
2022-10-11 13:26:11 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+
+export default function App() {
+    return(
+        <div>
+        <h1>properties</h1>
+        <h6>month: 2398238 year:3289328989 ytd: 2389</h6>
+        <Card address = {"18572 cull canyon rd"}/>
+        <Card address = {"929 lee ave"}/>
+        <Card address = {"800 marina blvd"}/>
+        <Card address = {"1915 E14 St"}/>
+        <Card address = {"18572 cull canyon rd"}/>
+        <Card address = {"929 lee ave"}/>
+        <Card address = {"800 marina blvd"}/>
+        <Card address = {"1915 E14 St"}/>
+        </div>
+    )
+}
