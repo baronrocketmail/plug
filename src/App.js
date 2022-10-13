03466810471c45b08811f0295c62137aed85405f
sammy freddie <sammyfreddie@sammys-MacBook-Pro.local>
2022-10-13 12:12:29 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import Card from "./Card";
 
-
 export default function App() {
     return(
         <div>
         <h1>properties</h1>
         <h6>month: 2398238 year:3289328989 ytd: 2389</h6>
-        <Card address = {"18572 cull canyon rd"}/>
-        <Card address = {"929 lee ave"}/>
-        <Card address = {"800 marina blvd"}/>
-        <Card address = {"1915 E14 St"}/>
-        <Card address = {"18572 cull canyon rd"}/>
-        <Card address = {"929 lee ave"}/>
-        <Card address = {"800 marina blvd"}/>
-        <Card address = {"1915 E14 St"}/>
+        <Card address = {"36 38 castro"}/>
+        <Card address = {"817 825 rodney"}/>
+        <Card address = {"539 blossom"}/>
+        <Card address = {"61 castro"}/>
         </div>
     )
 }
